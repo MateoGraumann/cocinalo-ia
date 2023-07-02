f8e8eb4c4c24bcd692107c5c54b78b971c21b251
Mateo <mateo@hoffentech.com>
2023-07-02 15:51:08 -0300

    Add Home Page

---
+++ cocinalo/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import '../styles/Home.css'
+import ImageGirl from '../media/girl_image.png'
+
+export default function Home(){
+    return(
+        <div className='Home-container'>
+            <div className="Home-content-image-and-description">
+                <div className="Home-description-container">
+                    <p>¿No sabes que cocinar?</p>
+                    <p>Deja que una IA lo haga por ti</p>
+                    <a href='#' className='Cook-btn'>Comenzar a cocinar</a>
+                </div>
+                <div className="Home-image-container">
+                    <img src={ImageGirl} alt='Chica_cocinando' />
+                </div>
+            </div>
+        </div>
+    )
+}
